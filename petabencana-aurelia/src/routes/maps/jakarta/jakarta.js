4080ae91d3d14dd43711bddaff992c72321e64f7
Abraham Quintero <abrahamquintero2323@gmail.com>
2016-11-22 06:59:20 -0500

    Initial commit with working api mocking and one get call

--- petabencana-aurelia/src/routes/maps/jakarta/jakarta.js
+++ petabencana-aurelia/src/routes/maps/jakarta/jakarta.js
@@ -1,3 +1,15 @@
+
+import {inject} from 'aurelia-framework';
+import {ReportcardApi} from 'ReportcardApi';
+
+var api = new ReportcardApi();
+
 export class Jakarta {
+  attached() {
+    this.api = api;
+    this.api.getAllReports()
+      .then( response =>
+            this.reports = response);
+  }
   
 }
