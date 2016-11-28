4f51098a0b242351016fdbbcd235c386bed74a23
ojha-url <mayank.ojha@gmail.com>
2016-11-28 13:47:20 -0500

    Completed review & submit card with msg passing

--- petabencana-aurelia/src/routes/cards/cards.js
+++ petabencana-aurelia/src/routes/cards/cards.js
@@ -19,9 +19,9 @@ export class Cards {
       {route: 'depth',        moduleId: './depth/depth',              settings: {title: this.i18n.tr('depth_title'),        cardNo: 2,  msgName: 'changedDepth'}},
       {route: 'photo',        moduleId: './photo/photo',              settings: {title: this.i18n.tr('photo_title'),        cardNo: 3,  msgName: 'changedPhoto'}},
       {route: 'description',  moduleId: './description/description',  settings: {title: this.i18n.tr('description_title'),  cardNo: 4,  msgName: 'changedDescription'}},
-      {route: 'review',       moduleId: './review/review',            settings: {title: this.i18n.tr('review_title'),       cardNo: 5,  msgName: 'getInputs'}},
+      {route: 'review',       moduleId: './review/review',            settings: {title: this.i18n.tr('review_title'),       cardNo: 5,  msgName: 'changedInputs'}},
       {route: 'terms',        moduleId: './terms/terms',              settings: {title: this.i18n.tr('terms_title'),        cardNo: 6}},
-      {route: 'thanks',        moduleId: './thanks/thanks',           settings: {title: this.i18n.tr('thanks_title'),       cardNo: 7}}
+      {route: 'thanks',       moduleId: './thanks/thanks',            settings: {title: this.i18n.tr('thanks_title'),       cardNo: 7}}
     ]);
     this.router = router;
   }
@@ -29,15 +29,28 @@ export class Cards {
     this.id = params.id;
   }
   attached() {
-    this.totalCards = this.router.routes.length - 1; //exclude {route:'', redirect:'location')
-    this.tabCount = [];
-    for (let i = 0; i < this.totalCards - 2; i += 1) {
-      this.tabCount[i] = i;
-      this.ea.subscribe(this.router.routes[i+1].settings.msgName, msg => { //TODO Replace with ReportCard object
-        this.router.routes[i+1].settings.input = msg;
-      });
-    }
     this.ccHeight = $(window).height() - ($('#cardTitle').height() + $('#cardNavigation').height());
+    this.totalCards = this.router.routes.length - 1; //exclude (route:'', redirect:'location')
+    this.tabCount = [];
+    for (let i = 0; i < this.totalCards - 2; i += 1) { //exclude terms & thanks cards
+      this.tabCount[i] = i;
+    }
+    this.ea.subscribe(this.router.routes[1].settings.msgName, msg => {
+      this.router.routes[1].settings.input = msg;
+      this.router.routes[5].settings.location = msg;
+    });
+    this.ea.subscribe(this.router.routes[2].settings.msgName, msg => {
+      this.router.routes[2].settings.input = msg;
+      this.router.routes[5].settings.depth = msg;
+    });
+    this.ea.subscribe(this.router.routes[3].settings.msgName, msg => {
+      this.router.routes[3].settings.input = msg;
+      this.router.routes[5].settings.photo = msg;
+    });
+    this.ea.subscribe(this.router.routes[4].settings.msgName, msg => {
+      this.router.routes[4].settings.input = msg;
+      this.router.routes[5].settings.description = msg;
+    });
   }
 
   get count() { //TODO navigation does not work unless getter is called from the DOM or elsewhere in js;
--- petabencana-aurelia/src/routes/cards/thanks/thanks.js
+++ petabencana-aurelia/src/routes/cards/thanks/thanks.js
@@ -1,3 +1,7 @@
 export class Thanks {
-  
+  attached() {
+    window.setTimeout(function () {
+      window.location.replace('/#/map');
+    }, 2500);
+  }
 }
