6b8ddce724f1238f84dabcade7dfc32910f73f7a
ojha-url <mayank.ojha@gmail.com>
2016-11-30 17:18:33 -0500

    Implemented cities and layer toggle using Layers class

--- petabencana-aurelia/src/routes/map/layers.js
+++ petabencana-aurelia/src/routes/map/layers.js
@@ -3,29 +3,50 @@ let DATASERVER = 'https://raw.githubusercontent.com/urbanriskmap/sampledata/mast
 
 // PetaBencana.id Layers class - manage leaflet data layers
 export class Layers {
-
-  constructor(leafletMap){
-    this.map = leafletMap
-    this.data = new Data; // Data class
+  constructor(leafletMap) {
+    this.map = leafletMap;
+    this.data = new Data(); // Data class
     this.layers = {}; // Layers
+    this.popupContent = [];
   }
 
   // Get flood reports as topojson, return Leaflet geojson layer
-  addReports(city_name){
-
+  addReports(city_name, openPane) {
     let url = DATASERVER + 'reports.' + city_name + '.geojson';
-
-    this.layers.reports = L.geoJSON();
     var that = this;
 
     return this.data.getData(url)
-      .then(function(data){
-        that.layers.reports.addData(data).addTo(that.map);
-      })
-      .catch((err) => console.log(err))
+    .then(function(data) {
+      that.layers.reports = L.geoJSON(data, {
+        onEachFeature: function(feature, layer) {
+          layer.on({
+            click: function() {
+              openPane();
+              that.popupContent = [];
+              for (let prop in feature.properties) {
+                that.popupContent.push(prop);
+              }
+            }
+          });
+        },
+        pointToLayer: function(feature, latlng) {
+          return L.marker(latlng, {
+            icon: L.icon({
+              iconUrl: 'assets/icons/marker.svg',
+              iconSize: [30, 30],
+              iconAnchor: [15, 15]
+            })
+          });
+        }
+      });
+      return that.layers.reports
+      .addTo(that.map);
+    }).catch((err) => console.log(err));
   }
 
   removeReports(){
-    if (this.layers.reports){this.map.removeLayer(this.layers.reports)}
+    if (this.layers.reports) {
+      this.map.removeLayer(this.layers.reports);
+    }
   }
 }
