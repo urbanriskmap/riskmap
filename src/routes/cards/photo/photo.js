6ae55676a8a7dac364ce9bd8d4dc4737c84fed08
ojha-url <mayank.ojha@gmail.com>
2016-12-13 15:10:05 -0500

    Fixed photo card bug

--- src/routes/cards/photo/photo.js
+++ src/routes/cards/photo/photo.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import {Reportcard} from 'Reportcard';
 var wrapper;
 var cntxt;
-var degree = 90;
+var degree = 0;
 
 //start-non-standard
 @inject(Reportcard)
@@ -22,60 +22,61 @@ export class Photo {
     wrapper = this.preview;
     cntxt = wrapper.getContext('2d');
     if (this.haveImg) {
-      this.drawImage();
+      this.drawImage(degree);
       $('#rotateButton').prop("disabled", false);
       $('#deleteButton').prop("disabled", false);
     }
   }
 
   sendClick() {
-    $('#photoCapture').trigger('click');
+    $('#ghostButton').trigger('click');
   }
 
-  drawImage(degree) {
-    if (this.selectedPhoto[0]) {
-      this.reportcard.setphoto(this.selectedPhoto);
-      wrapper.width = $('#canvas').width();
-      wrapper.height = $('#canvas').height();
-      let reader = new FileReader();
-      reader.onload = (e) => {
-        let reviewImg = new Image();
-        reviewImg.onload = () => {
-          let imgW;
-          let imgH;
-          let trlX = -wrapper.width/2;
-          let trlY = -wrapper.height/2;
-          if (reviewImg.width >= reviewImg.height) {
-            imgH = wrapper.height;
-            imgW = Math.round((reviewImg.width * imgH) / reviewImg.height);
-            trlX = trlX + Math.round((wrapper.width - imgW) / 2);
-          } else {
-            imgW = wrapper.width;
-            imgH = Math.round((reviewImg.height * imgW) / reviewImg.width);
-            trlY = trlY + Math.round((wrapper.height - imgH) / 2);
-          }
-          cntxt.translate(wrapper.width / 2, wrapper.height / 2);
-          cntxt.rotate(degree * Math.PI / 180);
-          cntxt.drawImage(reviewImg, trlX, trlY, imgW, imgH);
-        };
-        reviewImg.src = e.target.result;
+  drawImage(deg) {
+    this.reportcard.setphoto(this.selectedPhoto);
+    wrapper.width = $('#canvas').width();
+    wrapper.height = $('#canvas').height();
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      let reviewImg = new Image();
+      reviewImg.onload = () => {
+        let imgW;
+        let imgH;
+        let trlX = -wrapper.width/2;
+        let trlY = -wrapper.height/2;
+        if (reviewImg.width >= reviewImg.height) {
+          imgH = wrapper.height;
+          imgW = Math.round((reviewImg.width * imgH) / reviewImg.height);
+          trlX = trlX + Math.round((wrapper.width - imgW) / 2);
+        } else {
+          imgW = wrapper.width;
+          imgH = Math.round((reviewImg.height * imgW) / reviewImg.width);
+          trlY = trlY + Math.round((wrapper.height - imgH) / 2);
+        }
+        cntxt.translate(wrapper.width / 2, wrapper.height / 2);
+        cntxt.rotate(deg * Math.PI / 180);
+        cntxt.drawImage(reviewImg, trlX, trlY, imgW, imgH);
       };
-      reader.readAsDataURL(this.selectedPhoto[0]);
-      $('#rotateButton').prop("disabled", false);
-      $('#deleteButton').prop("disabled", false);
-    }
+      reviewImg.src = e.target.result;
+    };
+    $('#rotateButton').prop("disabled", false);
+    $('#deleteButton').prop("disabled", false);
+    reader.readAsDataURL(this.selectedPhoto[0]);
+    degree = deg;
+  }
+
+  rotatePhoto() {
+    degree += 90;
+    this.drawImage(degree);
   }
 
   deletePhoto() {
     cntxt.translate(-wrapper.width / 2, -wrapper.height / 2);
     cntxt.clearRect(0, 0, wrapper.width, wrapper.height);
+    this.selectedPhoto = null;
     this.reportcard.setphoto(null);
     $('#rotateButton').prop("disabled", true);
     $('#deleteButton').prop("disabled", true);
-  }
-
-  rotatePhoto() {
-    this.drawImage(degree);
-    degree += 90;
+    degree = 0;
   }
 }
