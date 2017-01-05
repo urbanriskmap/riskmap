85026665630666ca18f23319e44f53120358cba5
Tomas <tomasholderness@gmail.com>
2017-01-05 18:37:24 -0500

    Removed environment.js from git index

--- src/environment.js
+++
@@ -1,7 +0,0 @@
-export default {
-  debug: true,
-  testing: true,
-  tile_layer: 'http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}',
-  data_server: 'https://data-dev.petabencana.id/',
-  app: 'https://dev.petabencana.id'
-};
