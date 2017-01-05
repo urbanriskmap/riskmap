b355207c58f0d753666f0c4a65c5a7f2f9b97f2e
Tomas <tomasholderness@gmail.com>
2017-01-05 18:45:56 -0500

    Changed stamen tiles to https

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -1,7 +1,7 @@
 export default {
   debug: true,
   testing: true,
-  tile_layer: 'http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}',
+  tile_layer: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}',
   data_server: 'https://data-dev.petabencana.id/',
   app: 'https://dev.petabencana.id'
 };
