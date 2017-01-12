4c82073a3a571a30d6417c2b1e0066d12e4bf355
Tomas <tomasholderness@gmail.com>
2017-01-12 13:57:40 -0500

    Fix #121 - social media links

--- src/components/report-info/report-info.js
+++ src/components/report-info/report-info.js
@@ -19,9 +19,9 @@ export class ReportInfo {
 
   attached() {
     if (this.pkey) {
-      this.twitterText = "https://twitter.com/intent/tweet?text=" + messageText + "%20"+ env.app + this.city + "/" + this.pkey;
-      this.whatsappText = "whatsapp://send?text=" + messageText + "%20" + env.app + this.city + "/" + this.pkey;
-      this.facebookText = "http://www.facebook.com/sharer/sharer.php?u=" + env.app + this.city + "/" + this.pkey;
+      this.twitterText = "https://twitter.com/intent/tweet?text=" + messageText + "%20"+ env.app + "map/" + this.city + "/" + this.pkey;
+      this.whatsappText = "whatsapp://send?text=" + messageText + "%20" + env.app + "map/" + this.city + "/" + this.pkey;
+      this.facebookText = "http://www.facebook.com/sharer/sharer.php?u=" + env.app + "map/" + this.city + "/" + this.pkey;
     }
   }
 }
