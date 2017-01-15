3e815eab9a756f307ce074edfb7ea508ff646092
Tomas <tomasholderness@gmail.com>
2017-01-15 13:44:43 -0500

    Changed dev tile layer to mapbox streets

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -1,7 +1,8 @@
 export default {
   debug: true,
   testing: true,
-  tile_layer: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}',
+  //tile_layer:'https://api.mapbox.com/styles/v1/urbanriskmap/ciwwgpt9j004a2prwm9cylsrc/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNpdmVhbTFraDAwNHIyeWw1ZDB6Y2hhbTYifQ.tpgt1PB5lkJ-wITS02c96Q',
+  tile_layer: 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNpdmVhbTFraDAwNHIyeWw1ZDB6Y2hhbTYifQ.tpgt1PB5lkJ-wITS02c96Q',
   data_server: 'https://data.petabencana.id/',
   app: 'https://dev.petabencana.id/',
   default_language: 'en',
