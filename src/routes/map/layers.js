d2fc5eaaa807650e5867181b4fa14e1445f518ef
ojha-url <mayank.ojha@gmail.com>
2017-01-21 21:57:41 -0500

    Commit before merge

--- src/routes/map/layers.js
+++ src/routes/map/layers.js
@@ -20,12 +20,12 @@ export class Layers {
 
     return new Promise((resolve, reject) => {
       self.data.getData(url)
-        .then((data) => {
-          self.reports.addData(data);
-          var report = self.pkeyList[data.features[0].properties.pkey];
-          resolve(report);
-        })
-        .catch(() => reject(null));
+      .then((data) => {
+        self.reports.addData(data);
+        var report = self.pkeyList[data.features[0].properties.pkey];
+        resolve(report);
+      })
+      .catch(() => reject(null));
     });
   }
 
@@ -95,7 +95,7 @@ export class Layers {
               self.popupContent = {};
               let localTimestamp = new Date(feature.properties.created_at);
               // Create local timestamp
-              self.popupContent['timestamp'] = (localTimestamp.toLocaleDateString('id', {timeZone: "Asia/Jakarta"} ) + " " + localTimestamp.toLocaleTimeString('en', { hour12: false, timeZone: "Asia/Jakarta" }));
+              self.popupContent.timestamp = (localTimestamp.toLocaleDateString('id', {timeZone: "Asia/Jakarta"} ) + " " + localTimestamp.toLocaleTimeString('en', { hour12: false, timeZone: "Asia/Jakarta" }));
 
               for (let prop in feature.properties) {
                 self.popupContent[prop] = feature.properties[prop];
@@ -151,10 +151,10 @@ export class Layers {
     self.flood_extents = L.geoJSON(null, {
       style: function(feature, layer){
         switch (feature.properties.state) {
-    			case 4: return {fillColor:"#CC2A41",weight:1,color:"#CC2A41", opacity:0.8,fillOpacity: 0.8};
-    			case 3: return {fillColor:"#FF8300",weight:1,color:"#FF8300", opacity:0.8,fillOpacity: 0.8};
-    			case 2: return {fillColor:"#FFFF00",weight:1,color:"#FFFF00", opacity:0.8,fillOpacity: 0.8};
-    			case 1: return {fillColor:"#A0A9F7", weight:1,color:"#A0A9F7",opacity:0.8,fillOpacity: 0.8};
+          case 4: return {fillColor:"#CC2A41",weight:1,color:"#CC2A41", opacity:0.8,fillOpacity: 0.8};
+          case 3: return {fillColor:"#FF8300",weight:1,color:"#FF8300", opacity:0.8,fillOpacity: 0.8};
+          case 2: return {fillColor:"#FFFF00",weight:1,color:"#FFFF00", opacity:0.8,fillOpacity: 0.8};
+          case 1: return {fillColor:"#A0A9F7", weight:1,color:"#A0A9F7",opacity:0.8,fillOpacity: 0.8};
           default: return {color:"rgba(0,0,0,0)",weight:0,fillOpacity:0};
         }
       }
@@ -202,13 +202,13 @@ export class Layers {
       self.gaugeIcons = function(level){
         switch (level) {
           case 1:
-            return {'color':'#FF4000','icon':'assets/icons/floodgauge_1.png'};
+          return {'color':'#FF4000','icon':'assets/icons/floodgauge_1.png'};
           case 2:
-            return {'color':'#FF8000','icon':'assets/icons/floodgauge_2.png'};
+          return {'color':'#FF8000','icon':'assets/icons/floodgauge_2.png'};
           case 3:
-            return {'color':'#F7D358','icon':'assets/icons/floodgauge_3.png'};
+          return {'color':'#F7D358','icon':'assets/icons/floodgauge_3.png'};
           default:
-            return {'color':'#01DF01','icon':'assets/icons/floodgauge.png'};
+          return {'color':'#01DF01','icon':'assets/icons/floodgauge.png'};
         }
       };
 
@@ -216,13 +216,13 @@ export class Layers {
       self.gaugeLayer = L.geoJSON(null, {
         pointToLayer: (feature, latlng) => {
           return L.marker(latlng, {
-                  icon: L.icon({
-                    iconUrl: this.gaugeIcons(feature.properties.observations[feature.properties.observations.length-1].f3).icon,
-                    iconSize: [22,22],
-      							iconAnchor: [11, 11],
-      							popupAnchor: [0, 0]
-                  })
-                })
+            icon: L.icon({
+              iconUrl: this.gaugeIcons(feature.properties.observations[feature.properties.observations.length-1].f3).icon,
+              iconSize: [22,22],
+              iconAnchor: [11, 11],
+              popupAnchor: [0, 0]
+            })
+          });
         },
         onEachFeature: (feature, layer) => {
           layer.on({
@@ -241,51 +241,51 @@ export class Layers {
               }
 
               //TODO - set via Aurelia binding
-              $('#chart-title').html(feature.properties.gaugenameid)
+              $('#chart-title').html(feature.properties.gaugenameid);
               $('#chart-pane').html('<canvas id="modalChart"></canvas>');
 
               $('#modalChart').empty();
               var ctx = $('#modalChart').get(0).getContext("2d");
 
-    					var data = {
-    						labels : [],
-    						datasets : [{
-    							label: "Tinggi Muka Air / Water Depth (cm)",
-    							backgroundColor: "rgba(151,187,205,0.2)",
-    							borderColor: "rgba(151,187,205,1)",
-    							pointBackgroundColor: "rgba(151,187,205,1)",
-    							pointBorderColor: "#fff",
-    	            pointRadius: 4,
-    							data: [1,2,3]
-    						}]
-    					};
-    					for (var i = 0; i < feature.properties.observations.length; i++){
-    						data.labels.push(feature.properties.observations[i].f1);
-    						data.datasets[0].data.push(feature.properties.observations[i].f2);
-    					}
-    					var gaugeChart = new Chart(ctx,
-    					{type: 'line',
-    					data:data,
-    					options: {
-                bezierCurve:true,
-                legend: {display:true},
-                scaleLabel: "<%= ' ' + value%>",
-                scales: {
-                  xAxes: [{
-                    type: 'time',
-                    time: {
-                      unit: 'hour',
-                      unitStepSize: 1,
-                      displayFormats: {
-                        'millisecond': 'HH:mm',
-                				'second': 'HH:mm',
-  											'minute': 'HH:mm',
-  											'hour': 'HH:mm',
-  											'day': 'HH:mm',
-  											'week': 'HH:mm',
-  											'month': 'HH:mm',
-  											'quarter': 'HH:mm',
-  											'year': 'HH:mm'
+              var data = {
+                labels : [],
+                datasets : [{
+                  label: "Tinggi Muka Air / Water Depth (cm)",
+                  backgroundColor: "rgba(151,187,205,0.2)",
+                  borderColor: "rgba(151,187,205,1)",
+                  pointBackgroundColor: "rgba(151,187,205,1)",
+                  pointBorderColor: "#fff",
+                  pointRadius: 4,
+                  data: [1,2,3]
+                }]
+              };
+              for (var i = 0; i < feature.properties.observations.length; i++){
+                data.labels.push(feature.properties.observations[i].f1);
+                data.datasets[0].data.push(feature.properties.observations[i].f2);
+              }
+              var gaugeChart = new Chart(ctx,
+                {type: 'line',
+                data:data,
+                options: {
+                  bezierCurve:true,
+                  legend: {display:true},
+                  scaleLabel: "<%= ' ' + value%>",
+                  scales: {
+                    xAxes: [{
+                      type: 'time',
+                      time: {
+                        unit: 'hour',
+                        unitStepSize: 1,
+                        displayFormats: {
+                          'millisecond': 'HH:mm',
+                          'second': 'HH:mm',
+                          'minute': 'HH:mm',
+                          'hour': 'HH:mm',
+                          'day': 'HH:mm',
+                          'week': 'HH:mm',
+                          'month': 'HH:mm',
+                          'quarter': 'HH:mm',
+                          'year': 'HH:mm'
                         }
                       }
                     }]
@@ -293,13 +293,13 @@ export class Layers {
                   tooltips:{
                     enabled: false
                   }
-    						}
-    					});
+                }
+              });
               showPane('#chartPane');
             }
           });
         }
-      })
+      });
     }
 
     // Get areas where flooding is happening
@@ -331,5 +331,4 @@ export class Layers {
       self.gaugeLayer = null;
     }
   }
-
 }
