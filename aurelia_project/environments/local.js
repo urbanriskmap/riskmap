c5a5b4219ec193154c5fe6102567473182ee17ed
Tomas <tomasholderness@gmail.com>
2017-01-23 10:01:00 -0500

    Added local environment

---
+++ aurelia_project/environments/local.js
@@ -0,0 +1,9 @@
+export default {
+  debug: true,
+  testing: true,
+  tile_layer: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}',
+  data_server: 'http://localhost:8001/',
+  app: 'http://localhost:9000/',
+  default_language: 'en',
+  supported_languages: ['en', 'id']
+};
