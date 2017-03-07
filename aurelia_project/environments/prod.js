598918b128cf3d1e4e4870d72995e0cee6bfc790
Matthew Berryman <mattjb@me.com>
2017-03-07 14:40:18 +1100

    Revert "enable testing in prod"
    This reverts commit ff8b36177763a3d87c129c19527bfde13a0cb341.

--- aurelia_project/environments/prod.js
+++ aurelia_project/environments/prod.js
@@ -1,6 +1,6 @@
 export default {
   debug: false,
-  testing: true,
+  testing: false,
   tile_layer: 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNpdmVhbTFraDAwNHIyeWw1ZDB6Y2hhbTYifQ.tpgt1PB5lkJ-wITS02c96Q',
   data_server: 'https://data.petabencana.id/',
   app: 'https://petabencana.id/',
