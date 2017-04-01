7b47a2c223043acc2493c644c5b68da4deb1752c
Matthew Berryman <mattjb@me.com>
2017-04-01 17:05:07 +1100

    fixes #188

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -1,10 +1,10 @@
 export default {
   debug: true,
   testing: true,
-  tile_layer: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}',
+  tile_layer: 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNpdmVhbTFraDAwNHIyeWw1ZDB6Y2hhbTYifQ.tpgt1PB5lkJ-wITS02c96Q',
   data_server: 'https://data-dev.petabencana.id/',
   app: 'https://dev.petabencana.id/',
   default_language: 'en',
   supported_languages: ['en', 'id'],
-  enable_test_cardid: true 
+  enable_test_cardid: true
 };
