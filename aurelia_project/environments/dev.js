7e5e7fc5667d4774b7640664be6de86be9e083f3
Matthew Berryman <matthew@acrossthecloud.net>
2017-05-18 11:44:09 +1000

    change dev environment options

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -1,6 +1,6 @@
 export default {
-  debug: false,
-  testing: false,
+  debug: true,
+  testing: true,
   tile_layer: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}',
   data_server: 'https://data-dev.riskmap.in/',
   app: 'https://dev.riskmap.in/',
