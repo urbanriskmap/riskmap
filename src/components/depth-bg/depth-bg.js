5a0850f00c2a1f4c8cdd3042bc994db417ffb650
ojha-url <ojha@mit.edu>
2017-08-22 16:23:04 -0400

    Removed cards code from repo, to be used for map front end code

--- src/components/depth-bg/depth-bg.js
+++
@@ -1,8 +0,0 @@
-import {containerless} from 'aurelia-framework';
-
-//start-non-standard
-@containerless()
-//end-non-standard
-export class DepthBg {
-  
-}
--- src/resources/config.js
+++ src/resources/config.js
@@ -7,12 +7,6 @@ import {noView} from 'aurelia-framework';
 //end-non-standard
 export class Config {
   constructor() {
-    this.cards = {
-      "data_server" : env[dep.name].data_server,
-      "tile_layer": env[dep.name].tile_layer,
-      "enable_test_cardid": env.enable_test_cardid
-    };
-
     this.map = dep.map;
     this.map.data_server = env[dep.name].data_server;
     this.map.tile_layer = env[dep.name].tile_layer;
