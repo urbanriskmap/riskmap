a43f597750592765edd811d9b1a59b44168e025a
asbarve <adityasbarve@gmail.com>
2017-10-30 14:40:49 -0400

    changes in sidepane

--- deployment_specific/rm/ds_components/side-pane/side-pane.js
+++ deployment_specific/rm/ds_components/side-pane/side-pane.js
@@ -29,65 +29,65 @@ export class SidePane {
     }
     this.locale = {};
 
-    this.seltab = "map"; //default tab to open
-    this.tabList = ["report", "map", "info"]; //elements match names of fontello icons
+    this.seltab = 'map'; //default tab to open
+    this.tabList = ['report', 'map', 'info']; //elements match names of fontello icons
     this.vidWrapperOpened = false;
     this.videos = [
       {
-        platform: "flood",
+        platform: 'flood',
         source: {
-          "en": "https://www.youtube.com/embed/8aFRL4CaUIs",
-          "ta": "https://www.youtube.com/embed/8aFRL4CaUIs",
-          "mr": "https://www.youtube.com/embed/8aFRL4CaUIs",
-          "kn": "https://www.youtube.com/embed/8aFRL4CaUIs"
+          'en': ' https://www.youtube.com/embed/8aFRL4CaUIs',
+          'ta': 'https://www.youtube.com/embed/8aFRL4CaUIs',
+          'mr': 'https://www.youtube.com/embed/8aFRL4CaUIs',
+          'kn': 'https://www.youtube.com/embed/8aFRL4CaUIs'
         }
       },
       {
-        platform: "twitter", //Match string to locale/*/translation.json > report_content.*
+        platform: 'twitter', //Match string to locale/*/translation.json > report_content.*
         source: {
-          "en": "https://www.youtube.com/embed/OB5dLtFxVWY",
-          "ta": "https://www.youtube.com/embed/OB5dLtFxVWY",
-          "mr": "https://www.youtube.com/embed/OB5dLtFxVWY",
-          "kn": "https://www.youtube.com/embed/OB5dLtFxVWY"
+          'en': 'https://www.youtube.com/embed/OB5dLtFxVWY',
+          'ta': 'https://www.youtube.com/embed/OB5dLtFxVWY',
+          'mr': 'https://www.youtube.com/embed/OB5dLtFxVWY',
+          'kn': 'https://www.youtube.com/embed/OB5dLtFxVWY'
         }
       },
       {
-        platform: "telegram",
+        platform: 'telegram',
         source: {
-          "en": "https://www.youtube.com/embed/Sp1JbFd9KhM",
-          "ta": "https://www.youtube.com/embed/Sp1JbFd9KhM",
-          "mr": "https://www.youtube.com/embed/Sp1JbFd9KhM",
-          "kn": "https://www.youtube.com/embed/Sp1JbFd9KhM"
+          'en': 'https://www.youtube.com/embed/Sp1JbFd9KhM',
+          'ta': 'https://www.youtube.com/embed/Sp1JbFd9KhM',
+          'mr': 'https://www.youtube.com/embed/Sp1JbFd9KhM',
+          'kn': 'https://www.youtube.com/embed/Sp1JbFd9KhM'
         }
       },
       {
-        platform: "facebook",
+        platform: 'facebook',
         source: {
-          "en": "https://www.youtube.com/embed/unz-qCNUJzU",
-          "ta": "https://www.youtube.com/embed/unz-qCNUJzU",
-          "mr": "https://www.youtube.com/embed/unz-qCNUJzU",
-          "kn": "https://www.youtube.com/embed/unz-qCNUJzU"
+          'en': 'https://www.youtube.com/embed/unz-qCNUJzU',
+          'ta': 'https://www.youtube.com/embed/unz-qCNUJzU',
+          'mr': 'https://www.youtube.com/embed/unz-qCNUJzU',
+          'kn': 'https://www.youtube.com/embed/unz-qCNUJzU'
         }
       }
     ];
     this.report_type = [
-      {text: {"en": "Flood report", "ta": "எச்சரிக்கை நிலை 1","mr": "पुराची नोंद","kn": "Flood report"}, icon: 'flood', icon_color: '#31aade'},
-      {text: {"en": "Blocked Drains", "ta": "வடிகால் அடைப்ப","mr": "तुंबलेले ड्रेन","kn": "Blocked Drains"}, icon: 'drain', icon_color: '#782a07'},
-      {text: {"en": "Desilting", "ta": "தூர் எடுப்பு","mr": "गाळाचा निचरा","kn": "Desilting"}, icon: 'desilting', icon_color: '#d14d11'},
-      {text: {"en": "Canal Repairs", "ta": "கால்வாய் பழுது","mr": "कालव्याची दुरुस्ती","kn": "Canal Repairs"}, icon: 'canalrepair', icon_color: '#ba870d'},
-      {text: {"en": "Fallen Tree", "ta": "உடைந்த மரம்","mr": "पडलेले झाड","kn": "Fallen Tree"}, icon: 'treeclearing', icon_color: '#006b3b'}
+      {text: {'en': 'Flood report', 'ta': 'எச்சரிக்கை நிலை 1', 'mr': 'पुराची नोंद', 'kn': 'Flood report'}, icon: 'flood', icon_color: '#31aade'},
+      {text: {'en': 'Blocked Drains', 'ta': 'வடிகால் அடைப்ப', 'mr': 'तुंबलेले ड्रेन', 'kn': 'Blocked Drains'}, icon: 'drain', icon_color: '#782a07'},
+      {text: {'en': 'Desilting', 'ta': 'தூர் எடுப்பு', 'mr': 'गाळाचा निचरा', 'kn': 'Desilting'}, icon: 'desilting', icon_color: '#d14d11'},
+      {text: {'en': 'Canal Repairs', 'ta': 'கால்வாய் பழுது', 'mr': 'कालव्याची दुरुस्ती', 'kn': 'Canal Repairs'}, icon: 'canalrepair', icon_color: '#ba870d'},
+      {text: {'en': 'Fallen Tree', 'ta': 'உடைந்த மரம்', 'mr': 'पडलेले झाड', 'kn': 'Fallen Tree'}, icon: 'treeclearing', icon_color: '#006b3b'}
     ];
     this.gauge_levels = [
-      {text: {"en": "Alert Level 1", "ta": "எச்சரிக்கை நிலை 1", "mr": "इशाऱ्याची पातळी 1", "kn": "Alert Level 1"}, icon: 'assets/icons/floodgauge_1.svg'},
-      {text: {"en": "Alert Level 2", "ta": "எச்சரிக்கை நிலை 2", "mr": "इशाऱ्याची पातळी 2", "kn": "Alert Level 2"}, icon: 'assets/icons/floodgauge_2.svg'},
-      {text: {"en": "Alert Level 3", "ta": "எச்சரிக்கை நிலை 3", "mr": "इशाऱ्याची पातळी 3", "kn": "Alert Level 3"}, icon: 'assets/icons/floodgauge_3.svg'},
-      {text: {"en": "Alert Level 4", "ta": "எச்சரிக்கை நிலை 4", "mr": "इशाऱ्याची पातळी 4", "kn": "Alert Level 4"}, icon: 'assets/icons/floodgauge_4.svg'}
+      {text: {'en': 'Alert Level 1', 'ta': 'எச்சரிக்கை நிலை 1', 'mr': 'इशाऱ्याची पातळी 1', 'kn': 'Alert Level 1'}, icon: 'assets/icons/floodgauge_1.svg'},
+      {text: {'en': 'Alert Level 2', 'ta': 'எச்சரிக்கை நிலை 2', 'mr': 'इशाऱ्याची पातळी 2', 'kn': 'Alert Level 2'}, icon: 'assets/icons/floodgauge_2.svg'},
+      {text: {'en': 'Alert Level 3', 'ta': 'எச்சரிக்கை நிலை 3', 'mr': 'इशाऱ्याची पातळी 3', 'kn': 'Alert Level 3'}, icon: 'assets/icons/floodgauge_3.svg'},
+      {text: {'en': 'Alert Level 4', 'ta': 'எச்சரிக்கை நிலை 4', 'mr': 'इशाऱ्याची पातळी 4', 'kn': 'Alert Level 4'}, icon: 'assets/icons/floodgauge_4.svg'}
     ];
     this.flood_depth = [
-      {text: {"en": "> 150", "ta": "> 150","mr": "१५० से. मी. च्या वर","kn": "> 150"}, color: '#CC2A41'},
-      {text: {"en": "71 - 150", "ta": "71 - 150","mr": "७१ ते १५० से. मी.","kn": "71 - 150"}, color: '#FF8300'},
-      {text: {"en": "10 - 70", "ta": "10 - 70","mr": "१० ते ७० से. मी.","kn": "10 - 70"}, color: '#FFFF00'},
-      {text: {"en": "Use Caution", "ta": "எச்சரிக்கையுடன் செயல்படவும","mr": "सावधानीं बाळगा","kn": "Use Caution"}, color: '#A0A9F7'}
+      {text: {'en': '> 150', 'ta': '> 150', 'mr': '१५० से. मी. च्या वर', 'kn': '> 150'}, color: '#CC2A41'},
+      {text: {'en': '71 - 150', 'ta': '71 - 150', 'mr': '७१ ते १५० से. मी.', 'kn': '71 - 150'}, color: '#FF8300'},
+      {text: {'en': '10 - 70', 'ta': '10 - 70', 'mr': '१० ते ७० से. मी.', 'kn': '10 - 70'}, color: '#FFFF00'},
+      {text: {'en': 'Use Caution', 'ta': 'எச்சரிக்கையுடன் செயல்படவும', 'mr': 'सावधानीं बाळगा', 'kn': 'Use Caution'}, color: '#A0A9F7'}
     ];
   }
 
@@ -116,8 +116,8 @@ export class SidePane {
 
   switchTab(tab) {
     this.seltab = tab;
-    $('.tabLinks').removeClass("active");
-    $('#button-' + tab).addClass("active");
+    $('.tabLinks').removeClass('active');
+    $('#button-' + tab).addClass('active');
     if (tab === 'report' && !this.vidWrapperOpened) {
       $('#vid_' + this.videos[0].platform).ready(() => {
         this.showVideo(this.videos[0].platform);
@@ -133,10 +133,10 @@ export class SidePane {
   }
 
   showVideo(video) {
-    $('.videoWrapper:not(#vid_' + video + ')').slideUp("fast");
-    $('#vid_' + video).slideToggle("fast");
-    $('.labelRow:not(#label_' + video + ')').removeClass("active");
-    $('#label_' + video).toggleClass("active");
+    $('.videoWrapper:not(#vid_' + video + ')').slideUp('fast');
+    $('#vid_' + video).slideToggle('fast');
+    $('.labelRow:not(#label_' + video + ')').removeClass('active');
+    $('#label_' + video).toggleClass('active');
     $('#down_' + video + ', #up_' + video).toggle();
     $('.up:not(#up_' + video + ')').hide();
     $('.down:not(#down_' + video + ')').show();
