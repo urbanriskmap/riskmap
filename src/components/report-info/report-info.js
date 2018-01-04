22b8a55c1919a6d8ed1e9f95ad95e3e4155f3e58
asbarve <asbarve@users.noreply.github.com>
2018-01-04 13:03:41 -0500

    added points service, upvote downvote functional and styled

--- src/components/report-info/report-info.js
+++ src/components/report-info/report-info.js
@@ -1,9 +1,10 @@
 import {inject, bindable, customElement, computedFrom} from 'aurelia-framework';
 import {Config} from '../../resources/config';
+import {PointsService} from './points-service';
 
 //start-aurelia-decorators
 @customElement('report-info')
-@inject(Config)
+@inject(Config, PointsService)
 //end-aurelia-decorators
 export class ReportInfo {
   //@bindable attributes do not work with camelCase...
@@ -14,8 +15,9 @@ export class ReportInfo {
   @bindable id;
   //end-aurelia-decorators
 
-  constructor(Config) {
+  constructor(Config, PointsService) {
     this.config = Config;
+    this.service = PointsService;
     this.app = Config.map.app;
     this.links = {
       qlue: 'https://play.google.com/store/apps/details?id=org.qluein.android&hl=en',
@@ -31,13 +33,13 @@ export class ReportInfo {
     $('.voteButton').removeClass('active');
     $('#vote_button_' + button.name).addClass('active');
     switch (button.name) {
-    case 'share':
+      case 'share':
       $('#socialIcons').fadeToggle();
       break;
-    case 'flag':
+      case 'flag':
       $('#flagReport').fadeToggle();
       break;
-    default:
+      default:
       return null;
     }
   }
@@ -47,7 +49,6 @@ export class ReportInfo {
     let flagText = document.getElementById('flag_text').value;
     console.log(flagText);
   }
-
 
   get msgText() {
     return this.locale.report_info.share_msg;
@@ -63,6 +64,17 @@ export class ReportInfo {
   //start-aurelia-decorators
   @computedFrom('popupcontent')
   //end-aurelia-decorators
+  get reportId() {
+    if (this.popupcontent.pkey) {
+      return this.popupcontent.pkey;
+    } else {
+      return null;
+    }
+  }
+
+  //start-aurelia-decorators
+  @computedFrom('popupcontent')
+  //end-aurelia-decorators
   get height() {
     if (this.popupcontent.report_data) {
       return this.popupcontent.report_data.flood_depth;
@@ -70,6 +82,18 @@ export class ReportInfo {
       return null;
     }
   }
+
+  //start-aurelia-decorators
+  @computedFrom('popupcontent')
+  //end-aurelia-decorators
+  get points() {
+    if (this.popupcontent.report_data) {
+      return this.popupcontent.report_data.points;
+    } else {
+      return 0;
+    }
+  }
+
 
   //start-aurelia-decorators
   @computedFrom('popupcontent')
@@ -150,15 +174,7 @@ export class ReportInfo {
       }
     ];
 
-    self.voteButtons = [
-      {
-        'name': 'upvote',
-        'tooltip': 'Upvote this report'
-      },
-      {
-        'name': 'downvote',
-        'tooltip': 'Downvote this report'
-      },
+    self.shareButtons = [
       {
         'name': 'share',
         'tooltip': 'Share this report'
@@ -168,5 +184,94 @@ export class ReportInfo {
         'tooltip': 'Flag this report as inappropriate'
       }
     ];
+
+    self.voteButtons = [
+      {
+        'name': 'upvote',
+        'tooltip': 'Upvote this report',
+        'enabled': true
+      },
+      {
+        'name': 'downvote',
+        'tooltip': 'Downvote this report',
+        'enabled': true
+      }
+    ];
+  }
+
+  //start-aurelia-decorators
+  @computedFrom('popupcontent.voteChanged')
+  //end-aurelia-decorators
+  get upvoteDisabled() {
+    if (localStorage.getItem('id_' + this.reportId)) {
+      if (localStorage.getItem('id_' + this.reportId) === 'up') {
+        return 'disabled';
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  //start-aurelia-decorators
+  @computedFrom('popupcontent.voteChanged')
+  //end-aurelia-decorators
+  get downvoteDisabled() {
+    if (localStorage.getItem('id_' + this.reportId)) {
+      if (localStorage.getItem('id_' + this.reportId) === 'down') {
+        return 'disabled';
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  voteHandler(vote) {
+    const self = this;
+    self.service.updatePoints(self.reportId, vote)
+    .then(points => {
+      if (vote > 0) {
+        // Upvote
+        if (localStorage.getItem('id_' + self.reportId)) {
+          if (localStorage.getItem('id_' + this.reportId) === 'down') {
+            // Case 1: already downvoted
+            localStorage.setItem('id_' + self.reportId, 'none');
+          } else {
+            // Case 2: not downvoted
+            localStorage.setItem('id_' + self.reportId, 'up');
+          }
+        } else {
+          // Case 3: never voted for this report id
+          localStorage.setItem('id_' + self.reportId, 'up');
+        }
+
+        // Trigger getter to update disabled status
+        self.popupcontent.voteChanged = true;
+      } else {
+        // Downvote
+        if (localStorage.getItem('id_' + self.reportId)) {
+          if (localStorage.getItem('id_' + this.reportId) === 'up') {
+            // Case 1: already upvoted
+            localStorage.setItem('id_' + self.reportId, 'none');
+          } else {
+            // Case 2: not upvoted
+            localStorage.setItem('id_' + self.reportId, 'down');
+          }
+        } else {
+          // Case 3: never voted for this report id
+          localStorage.setItem('id_' + self.reportId, 'down');
+        }
+
+        // Trigger getter to update disabled status
+        self.popupcontent.voteChanged = true;
+      }
+      console.log(localStorage);
+    });
+
+    // Set voteChanged back to false to enable trigger on next button click
+    self.popupcontent.voteChanged = false;
   }
 }
