8ff17668178e7d0bca8a6f2e25390df0fd86bdf1
asbarve <adityasbarve@gmail.com>
2018-01-05 11:12:58 -0500

    added report id in flag report function, toggle for feature interaction fixed

--- src/components/report-info/report-info.js
+++ src/components/report-info/report-info.js
@@ -29,12 +29,13 @@ export class ReportInfo {
   }
 
   feedbackInteraction(button) {
-    $('.interactionFlyer').hide();
+    // $('.interactionFlyer').hide();
     $('.voteButton').removeClass('active');
     $('#vote_button_' + button.name).addClass('active');
     switch (button.name) {
       case 'share':
       $('#socialIcons').fadeToggle();
+
       break;
       case 'flag':
       $('#flagReport').fadeToggle();
@@ -43,6 +44,8 @@ export class ReportInfo {
       return null;
     }
   }
+
+
 
   submitFlag()
   {
