3133c152e736b1894e9e888a831bfc4e190adea5
DC <eopoxf@gmail.com>
2020-02-04 12:10:26 +0530

    Add initial set of regions in whole IDN

--- aurelia_project/deployments/pb.js
+++ aurelia_project/deployments/pb.js
@@ -2,8 +2,8 @@ export default {
   name: 'petabencana',
   height_units: 'cm',
   supported_languages: [
-    {key: 'en', name: 'English'},
-    {key: 'id', name: 'Bahasa'}
+    { key: 'en', name: 'English' },
+    { key: 'id', name: 'Bahasa' }
   ],
   map: {
     'instance_regions': {
@@ -34,18 +34,154 @@ export default {
           'sw': [-7.1167, 110.2584],
           'ne': [-6.8724, 110.6673]
         }
+      },
+      'Bali': {
+        'region': 'ID-BA',
+        'bounds': { 'sw': [-8.85, 114.432], 'ne': [-8.062, 115.712] }
+      },
+      'West Nusa Tenggara': {
+        'region': 'ID-NB',
+        'bounds': { 'sw': [-9.109, 115.821], 'ne': [-8.08, 119.346] }
+      },
+      'Banten': {
+        'region': 'ID-BT',
+        'bounds': { 'sw': [-7.016, 105.1], 'ne': [-5.808, 106.78] }
+      },
+      'Central Java': {
+        'region': 'ID-JT',
+        'bounds': { 'sw': [-8.212, 108.556], 'ne': [-5.726, 111.691] }
+      },
+      'West Java': {
+        'region': 'ID-JB',
+        'bounds': { 'sw': [-7.821, 106.371], 'ne': [-5.915, 108.838] }
+      },
+      'Central Kalimantan': {
+        'region': 'ID-KT',
+        'bounds': { 'sw': [-3.544, 110.733], 'ne': [0.795, 115.849] }
+      },
+      'South Kalimantan': {
+        'region': 'ID-KS',
+        'bounds': { 'sw': [-4.943, 114.351], 'ne': [-1.302, 117.458] }
+      },
+      'West Kalimantan': {
+        'region': 'ID-KB',
+        'bounds': { 'sw': [-3.068, 108.599], 'ne': [2.082, 114.207] }
+      },
+      'Central Sulawesi': {
+        'region': 'ID-ST',
+        'bounds': { 'sw': [-3.322, 119.422], 'ne': [1.374, 124.033] }
+      },
+      'Gorontalo': {
+        'region': 'ID-GO',
+        'bounds': { 'sw': [0.306, 121.156], 'ne': [1.041, 123.552] }
+      },
+      'North Sulawesi': {
+        'region': 'ID-SA',
+        'bounds': { 'sw': [0.292, 123.112], 'ne': [5.566, 127.164] }
+      },
+      'South Sulawesi': {
+        'region': 'ID-SN',
+        'bounds': { 'sw': [-7.76, 117.038], 'ne': [-1.891, 122.222] }
+      },
+      'Southeast Sulawesi': {
+        'region': 'ID-SG',
+        'bounds': { 'sw': [-6.213, 120.866], 'ne': [-2.77, 124.612] }
+      },
+      'West Sulawesi': {
+        'region': 'ID-SR',
+        'bounds': { 'sw': [-3.571, 118.757], 'ne': [-0.861, 119.876] }
+      },
+      'Aceh': {
+        'region': 'ID-AC',
+        'bounds': { 'sw': [1.976, 95.011], 'ne': [6.077, 98.286] }
+      },
+      'Bengkulu': {
+        'region': 'ID-BE',
+        'bounds': { 'sw': [-5.514, 100.635], 'ne': [-2.277, 103.781] }
+      },
+      'Jambi': {
+        'region': 'ID-JA',
+        'bounds': { 'sw': [-2.782, 101.123], 'ne': [-0.746, 104.491] }
+      },
+      'Lampung': {
+        'region': 'ID-LA',
+        'bounds': { 'sw': [-6.168, 103.593], 'ne': [-3.724, 105.916] }
+      },
+      'Riau': {
+        'region': 'ID-RI',
+        'bounds': { 'sw': [-1.12, 100.025], 'ne': [2.883, 103.815] }
+      },
+      'West Sumatra': {
+        'region': 'ID-SB',
+        'bounds': { 'sw': [-3.349, 98.596], 'ne': [0.907, 101.886] }
+      },
+      'South Sumatra': {
+        'region': 'ID-SS',
+        'bounds': { 'sw': [-4.924, 102.041], 'ne': [-1.627, 106.221] }
+      },
+      'North Sumatra': {
+        'region': 'ID-SU',
+        'bounds': { 'sw': [-0.639, 97.058], 'ne': [4.302, 100.571] }
+      },
+      'East Nusa Tenggara': {
+        'region': 'ID-NT',
+        'bounds': { 'sw': [-11.008, 118.927], 'ne': [-7.778, 125.193] }
+      },
+      'Maluku': {
+        'region': 'ID-MA',
+        'bounds': { 'sw': [-8.345, 125.723], 'ne': [-2.725, 134.909] }
+      },
+      'North Maluku': {
+        'region': 'ID-MU',
+        'bounds': { 'sw': [-2.477, 124.144], 'ne': [2.645, 129.657] }
+      },
+      'East Java': {
+        'region': 'ID-JI',
+        'bounds': { 'sw': [-8.781, 110.899], 'ne': [-5.043, 116.27] }
+      },
+      'Bangka-Belitung Islands': {
+        'region': 'ID-BB',
+        'bounds': { 'sw': [-3.341, 105.108], 'ne': [-0.909, 109.389] }
+      },
+      'Riau Islands': {
+        'region': 'ID-KR',
+        'bounds': { 'sw': [-0.87, 103.284], 'ne': [4.796, 109.168] }
+      },
+      'Papua': {
+        'region': 'ID-PA',
+        'bounds': { 'sw': [-9.127, 134.299], 'ne': [0.938, 141.02] }
+      },
+      'West Papua': {
+        'region': 'ID-PB',
+        'bounds': { 'sw': [-4.315, 129.3], 'ne': [1.081, 135.258] }
+      },
+      'East Kalimantan': {
+        'region': 'ID-KI',
+        'bounds': { 'sw': [-2.541, 113.836], 'ne': [2.569, 119.038] }
+      },
+      'North Kalimantan': {
+        'region': 'ID-KU',
+        'bounds': { 'sw': [1.114, 114.565], 'ne': [4.408, 117.991] }
+      },
+      'Special Region of Yogyakarta': {
+        'region': 'ID-YO',
+        'bounds': { 'sw': [-8.204, 110.003], 'ne': [-7.541, 110.839] }
+      },
+      'Jakarta Special Capital Region': {
+        'region': 'ID-JK',
+        'bounds': { 'sw': [-6.374, 106.39], 'ne': [-5.202, 106.974] }
       }
     },
     'default_region': {
       'region': 'java',
       'bounds': {
-        'sw': [-10.293, 104.677],
-        'ne': [-3.974, 115.290]
+        'sw': [-11.0076711, 95.0107985], // [ymin , xmin]
+        'ne': [6.076744, 141.0200345] // [ymax, xmax]
       }
     },
-    'region_center': [-7, 110],
+    'region_center': [-2.465463549999999, 118.01541650000001],
     'start_city_center': [-6.1754, 106.8271],
-    'starting_zoom': 8,
-    'minimum_zoom': 8
+    'starting_zoom': 6,
+    'minimum_zoom': 5
   }
 };
