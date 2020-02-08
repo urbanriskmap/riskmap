a182aa76854d0dd175bb114c02ec180b6e29b35b
DC <eopoxf@gmail.com>
2020-02-08 17:30:43 +0530

    Config oad cards url

--- src/resources/config.js
+++ src/resources/config.js
@@ -10,6 +10,7 @@ export class Config {
     this.map = dep.map;
     this.map.dep_name = dep.name;
     this.map.data_server = env[dep.name].data_server;
+    this.map.cards_server = env[dep.name].cards_server;
     this.map.tile_layer = env[dep.name].tile_layer;
     this.map.app = env[dep.name].app;
     this.map.report_timeperiod = env[dep.name].report_timeperiod;
