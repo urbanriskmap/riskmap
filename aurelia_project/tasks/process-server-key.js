23e0e6c8c9ba1e54dade24b560f1e14ec0bd70e1
DC <eopoxf@gmail.com>
2020-02-09 11:46:06 +0530

    Make the default server same as placehoder

--- aurelia_project/tasks/process-server-key.js
+++ aurelia_project/tasks/process-server-key.js
@@ -6,7 +6,8 @@ let replace = require('gulp-replace');
 
 export default function processServerKey() {
   let env = CLIOptions.getFlagValue('env', 'dev');
-  let serverKey = CLIOptions.getFlagValue('data_server_key', '');
+  let serverKey = CLIOptions.getFlagValue('data_server_key', '{data_server_key}');
+  if (!serverKey) serverKey = '{data_server_key}';
   return gulp.src(`aurelia_project/environments/${env}.js`)
     .pipe(changedInPlace({ firstPass: true }))
     .pipe(replace('{data_server_key}', serverKey))
